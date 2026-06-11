18b54e751ffbc6c62b14590fda37f02c56bffa61
DeveshK1256 <deveshkushwaha1256@gmail.com>
2026-06-11 21:01:00 +0530

    Fix last ESLint error: add Blob global

--- eslint.config.js
+++ eslint.config.js
@@ -40,6 +40,7 @@ export default [
         alert: "readonly",
         confirm: "readonly",
         performance: "readonly",
+        Blob: "readonly",
         google: "readonly",
       },
     },
